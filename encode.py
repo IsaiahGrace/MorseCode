f4db6ef4029b339a6cf85159904d6a9bcdea0042
Isaiah Grace <igrace@purdue.edu>
2018-02-07 17:45:20 -0500

    Converts to lowercase and strips whitespace

--- encode.py
+++ encode.py
@@ -1,3 +1,15 @@
-#! /usr/dev/env python3.4
+#! /usr/bin/env python
 
 #Encode given input string into a morse code string without spaces
+
+def encodeMorse(text):
+    text = text.lower()
+    words = text.split()
+    wordsList = [word.strip() for word in words]
+    text = ''.join(wordsList)
+    
+    return(text)
+
+
+if __name__ == "__main__":
+    print(encodeMorse("Test One"))
