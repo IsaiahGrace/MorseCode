29e31d7142665352de180cba0362d4f4a5a1f1db
Isaiah Grace <igrace@purdue.edu>
2018-02-07 18:38:18 -0500

    Changed name of morse dict to text_morse

--- encode.py
+++ encode.py
@@ -2,44 +2,45 @@
 
 #Encode given input string into a morse code string without spaces
 
+#Define dictionary to convert char to morse
+text_morse = {'a':'.-',
+              'b':'-...',
+              'c':'-.-.',
+              'd':'-..',
+              'e':'.',
+              'f':'..-.',
+              'g':'--.',
+              'h':'....',
+              'i':'..',
+              'j':'.---',
+              'k':'-.-',
+              'l':'.-..',
+              'm':'--',
+              'n':'-.',
+              'o':'---',
+              'p':'.--.',
+              'q':'--.-',
+              'r':'.-.',
+              's':'...',
+              't':'-',
+              'u':'..-',
+              'v':'...-',
+              'w':'.--',
+              'x':'-..-',
+              'y':'-.--',
+              'z':'--..',
+              '1':'.----',
+              '2':'..---',
+              '3':'...--',
+              '4':'....-',
+              '5':'.....',
+              '6':'-....',
+              '7':'--...',
+              '8':'---..',
+              '9':'----.',
+              '0':'-----'}
+
 def encodeMorse(text):
-    #Define dictionary to convert char to morse
-    morse = {'a':'.-',
-             'b':'-...',
-             'c':'-.-.',
-             'd':'-..',
-             'e':'.',
-             'f':'..-.',
-             'g':'--.',
-             'h':'....',
-             'i':'..',
-             'j':'.---',
-             'k':'-.-',
-             'l':'.-..',
-             'm':'--',
-             'n':'-.',
-             'o':'---',
-             'p':'.--.',
-             'q':'--.-',
-             'r':'.-.',
-             's':'...',
-             't':'-',
-             'u':'..-',
-             'v':'...-',
-             'w':'.--',
-             'x':'-..-',
-             'y':'-.--',
-             'z':'--..',
-             '1':'.----',
-             '2':'..---',
-             '3':'...--',
-             '4':'....-',
-             '5':'.....',
-             '6':'-....',
-             '7':'--...',
-             '8':'---..',
-             '9':'----.',
-             '0':'-----'}
     #prepare input string, convert to lowercase and strip whitespace
     text = text.lower()
     words = text.split()
@@ -48,13 +49,14 @@ def encodeMorse(text):
     code = []
     #iterate through the text and encode the characters
     for char in text:
-        if not char in morse.keys():
+        if not char in text_morse.keys():
             print("Error! invalid input, only letters and numbers allowed")
             return(None)
-        code.append(morse[char])
+        code.append(text_morse[char])
         
     return(''.join(code))
 
 
 if __name__ == "__main__":
-    print(encodeMorse("Test One"))
+    
+    print(encodeMorse("Test One Test Two Test Three"))
