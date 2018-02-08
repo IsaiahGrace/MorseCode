42b889154b0d564a52ff28a9def755cb1182397f
Isaiah Grace <igrace@purdue.edu>
2018-02-07 21:25:12 -0500

    findPossible() now returns a set of possible strings

--- decode.py
+++ decode.py
@@ -2,7 +2,9 @@
 
 #Decode an input morse code string that lacks spaces into meaningfull words
 
+import anytree as tree
 import encode
+
 #Define dictionary to convert morse to char
 morse_text = {'.-':'a',
               '-...':'b',
@@ -41,24 +43,38 @@ morse_text = {'.-':'a',
               '----.':'9',
               '-----':'0'}
 
+def buildTree(node, code):
+    for key in morse_text.keys():
+        if list(key) == code[:len(key)]:
+            newNode = tree.Node(morse_text[key])
+            newNode.parent = node
+            buildTree(newNode, code[len(key):])
+    return()
+
+def buildSolutions(solutions, node):
+    if node.is_leaf:
+        solution = ''
+        for ancestor in node.ancestors:
+            solution += ancestor.name
+        solution += node.name
+        solutions.add(solution)
+    else:
+        for child in node.children:
+            buildSolutions(solutions, child)
+    return(None)
 
 def findPossible(morse):
-    
-    solutions = []
+    solutions = set()
     charList = list(morse)
     for char in charList:
         if not char == '.' and not char == '-':
             print("Error! bad morse code, only include '.' and '-'")
             return(None)
-
-    ## The longest interpretation of the morse code will be all e and t with length = len(charList)
-    for count in range(0,len(charList)):
-        first = ''.join(charList[:len(charList) - count])
-        solution = morse_text.get(first,'')
-        if solution:
-            solutions.append(solution)
-            solution = ''
-        
+    root = tree.Node('')
+    buildTree(root, charList)
+    buildSolutions(solutions, root)
+    for pre,fill,node in tree.RenderTree(root):
+        print("%s%s" % (pre, node.name))    
     return(solutions)
 
 
