91c3ebecfb0aa87da0a73305042a1e7bf1a50316
Isaiah Grace <igrace@purdue.edu>
2018-02-08 00:29:02 -0500

    tried to optomize code, not sure if it's working

--- decode.py
+++ decode.py
@@ -7,6 +7,7 @@ import enchant
 import encode
 
 #Define dictionary to convert morse to char
+d = enchant.Dict("en_US")
 morse_text = {'.-':'a',
               '-...':'b',
               '-.-.':'c',
@@ -45,6 +46,8 @@ morse_text = {'.-':'a',
               '-----':'0'}
 
 def buildTree(node, code):
+    if not code:
+        return()
     for key in morse_text.keys():
         if list(key) == code[:len(key)]:
             newNode = tree.Node(morse_text[key])
@@ -58,14 +61,14 @@ def buildSolutions(solutions, node):
         for ancestor in node.ancestors:
             solution += ancestor.name
         solution += node.name
-        solutions.add(solution)
+        solutions.append(solution)
     else:
         for child in node.children:
             buildSolutions(solutions, child)
-    return(None)
+    return()
 
 def findPossible(morse):
-    solutions = set()
+    solutions = []
     charList = list(morse)
     for char in charList:
         if not char == '.' and not char == '-':
@@ -74,8 +77,8 @@ def findPossible(morse):
     root = tree.Node('')
     buildTree(root, charList)
     buildSolutions(solutions, root)
-    #for pre,fill,node in tree.RenderTree(root):
-    #    print("%s%s" % (pre, node.name))    
+    for pre,fill,node in tree.RenderTree(root):
+        print("%s%s" % (pre, node.name))    
     return(solutions)
 
 def findWords(solutions):
@@ -86,16 +89,41 @@ def findWords(solutions):
             words.add(word)
     return(words)
 
+def recurseDecode(words, node, code):
+    if not code:
+        solution =''
+        for ancestor in node.ancestors:
+            solution += ancestor.name
+        solution += node.name
+        if solution and d.check(solution):
+            words.add(solution)
+        return()
+    for key in morse_text.keys():
+        if list(key) == code[:len(key)]:
+            newNode = tree.Node(morse_text[key])
+            newNode.parent = node
+            recurseDecode(words, newNode, code[len(key):])    
+    return()
+    
+def decodeWords(code):
+    words = set()
+    root = tree.Node('')
+    recurseDecode(words, root, list(code))
+    return(words)
+
 if __name__ == "__main__":
     # L = .-..
     # Possible interpretations of .-.. are:
     # L, AI, ED, RE, AEE, ENE, ETI, ETEE
-    char = 'computer'
+    char = 'compute'
     code = encode.encodeMorse(char)
-    solutions = findPossible(code)
+    #solutions = findPossible(code)
     print("Origional input:")
     print(char + ': ' + code)
-    print("Number of possible solutions:")
-    print(len(solutions))
-    print("Words found:")
-    print(findWords(solutions))
+    #print("possible solutions:")
+    #print(len(solutions))
+    #print(solutions)
+    #print("Words found:")
+    #print(findWords(solutions))
+    print("decode function:")
+    print(decodeWords(code))
